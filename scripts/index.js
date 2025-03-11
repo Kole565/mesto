85dc245535b57c57efbe71db4cd2d4418e21cf43
Kole565 <rjkz565rjkz565@gmail.com>
2025-03-11 21:23:02 +0300

    Add cards loading.

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+function createCard(cardTitle, cardImageLink) {
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+	const cardTitleElement = cardElement.querySelector('.card__title');
+	const cardImageElement = cardElement.querySelector('.card__image');
+
+	cardTitleElement.textContent = cardTitle;
+	cardImageElement.src = cardImageLink;
+	cardImageElement.alt = cardTitle;
+
+	return cardElement;
+}
 
 // @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+placesList = document.querySelector(".places__list")
+for (let i = 0; i < initialCards.length; i++) {
+	placesList.append(createCard(initialCards[i].name, initialCards[i].link));
+}
