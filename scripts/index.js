a7160c0f8703cdb729c42f920d398afe0077ca73
Kole565 <rjkz565rjkz565@gmail.com>
2025-03-13 19:26:57 +0300

    feat: add profile editing

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,17 @@
 const cardTemplate = document.querySelector('#card-template').content;
 
+const profilePopup = document.querySelector('.popup_type_edit');
+const cardPopup = document.querySelector('.popup_type_new-card');
+const imagePopup = document.querySelector('.popup_type_image');
+
+
+function openModal(popup) {
+    popup.classList.add('popup_is-opened');
+}
+
+function closeModal(popup) {
+    popup.classList.remove('popup_is-opened'); // TODO: Test
+}
 
 function createCard(cardTitle, cardImageLink) {
 	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
