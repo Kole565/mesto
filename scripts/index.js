3590dbcc53d75a6c6c027ea258d6bf882632e54c
Kole565 <rjkz565rjkz565@gmail.com>
2025-03-14 23:03:22 +0300

    feat: add cards

--- scripts/index.js
+++ scripts/index.js
@@ -4,13 +4,89 @@ const profilePopup = document.querySelector('.popup_type_edit');
 const cardPopup = document.querySelector('.popup_type_new-card');
 const imagePopup = document.querySelector('.popup_type_image');
 
+const profileOpenButton = document.querySelector('.profile__edit-button');
+const cardAddButton = document.querySelector('.profile__add-button');
+
+const profileFormElement = profilePopup.querySelector('.popup__form');
+const nameInput = profileFormElement.querySelector('.popup__input_type_name');
+const jobInput = profileFormElement.querySelector('.popup__input_type_description');
+const profileNameElement = document.querySelector('.profile__title');
+const profileDescriptionElement = document.querySelector('.profile__description');
+const cardFormElement = cardPopup.querySelector('.popup__form');
+const cardNameInput = cardFormElement.querySelector('.popup__input_type_card-name');
+const cardURLInput = cardFormElement.querySelector('.popup__input_type_url');
+
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+cardFormElement.addEventListener('submit', handleCardFormSubmit);
+
 
 function openModal(popup) {
     popup.classList.add('popup_is-opened');
 }
 
 function closeModal(popup) {
-    popup.classList.remove('popup_is-opened'); // TODO: Test
+    popup.classList.remove('popup_is-opened');
+}
+
+profileOpenButton.addEventListener('click', function () {
+	openModal(profilePopup);
+
+	const popupCloseButton = profilePopup.querySelector('.popup__close');
+	popupCloseButton.addEventListener('click', function () {
+		closeModal(profilePopup);
+	});
+
+	prefillProfileForm();
+});
+
+function prefillProfileForm() {
+	const formName = profilePopup.querySelector('.popup__input_type_name');
+	const formDescription = profilePopup.querySelector('.popup__input_type_description');
+
+	formName.value = profileNameElement.textContent;
+	formDescription.value = profileDescriptionElement.textContent;
+}
+
+function handleProfileFormSubmit(evt) {
+	evt.preventDefault();
+
+	const name = nameInput.value;
+	const job = jobInput.value;
+
+	profileNameElement.textContent = name;
+	profileDescriptionElement.textContent = job;
+
+	closeModal(profilePopup);
+}
+
+cardAddButton.addEventListener('click', function () {
+	openModal(cardPopup);
+
+	const popupCloseButton = cardPopup.querySelector('.popup__close');
+	popupCloseButton.addEventListener('click', function () {
+		closeModal(cardPopup);
+	});
+
+	prefillCardForm();
+});
+
+function prefillCardForm() {
+	const formName = cardPopup.querySelector('.popup__input_type_card-name');
+	const formURL = cardPopup.querySelector('.popup__input_type_url');
+
+	formName.value = '';
+	formURL.value = '';
+}
+
+function handleCardFormSubmit(evt) {
+	evt.preventDefault();
+
+	const name = cardNameInput.value;
+	const url = cardURLInput.value;
+
+	placesList.prepend(createCard(name, url));
+
+	closeModal(cardPopup);
 }
 
 function createCard(cardTitle, cardImageLink) {
