b3c078f071c50b8b06a673d4b98258bd306dfcc2
Kole565 <rjkz565rjkz565@gmail.com>
2025-03-24 17:27:34 +0300

    feat(webpack): install postcss

--- webpack.config.js
+++ webpack.config.js
@@ -28,9 +28,13 @@ module.exports = {
 			{
 				test: /\.css$/,
 
-				use: [MiniCssExtractPlugin.loader, {
-					loader: 'css-loader'
-				}]
+				use: [MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: { importLoaders: 1 }
+					},
+					'postcss-loader'
+				]
 			}
 		]
   	},
